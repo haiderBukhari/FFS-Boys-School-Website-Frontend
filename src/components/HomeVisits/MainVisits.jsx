3507b5e080f442b1032027d304908fec0e859007
haiderBukhari <apshaiderbukhari786@gmail.com>
2023-12-24 10:54:58 +0500

    Image Properties Changes

--- src/components/HomeVisits/MainVisits.jsx
+++ src/components/HomeVisits/MainVisits.jsx
@@ -1,26 +1,43 @@
-import visit1 from "../../assets/img/visits/visit1.jpeg"
-import visit2 from "../../assets/img/visits/visit2.jpeg"
-import visit3 from "../../assets/img/visits/visit3.jpeg"
+import { useState } from "react"
+import visit1 from "../../assets/img/visits/Visit1/visit1.jpeg"
+import visit2 from "../../assets/img/visits/Visit2/visit2.jpeg"
+import visit21 from "../../assets/img/visits/Visit2/visit21.jpeg"
+import visit22 from "../../assets/img/visits/Visit2/visit22.jpeg"
+import visit23 from "../../assets/img/visits/Visit2/visit23.jpeg"
+import visit24 from "../../assets/img/visits/Visit2/visit24.jpeg"
+import visit3 from "../../assets/img/visits/Visit3/visit3.jpeg"
+import visit31 from "../../assets/img/visits/Visit3/visit31.jpeg"
+
 import { VisitComponent } from "./VisitComponent"
+import { VisitsPicturesDialog } from "./VisitsPicturesDialog"
+
 export const MainVisits = () => {
+    const [selected, setSelected] = useState(null);
+    const [dialog, setDialog] = useState(true);
     const data = [
         {
+            id: 1,
             date: "22 Aug, 2020",
             img: visit1,
             title: "Visit of Respected Ex Dir FF Edn Gen Naseer.",
-            text: "Honourable Ex Dir FF Edn Gen Naseer visiting FFS Boys Wah"
+            text: "Honourable Ex Dir FF Edn Gen Naseer visiting FFS Boys Wah",
+            images: [visit1]
         },
         {
+            id: 2,
             date: "22 Aug, 2020",
             img: visit2,
             title: "Visit of worthy GM Brig Asim.",
-            text: "Worthy GM Brig Asim SM, HR Col Noman Bukhari, DM edu Zahoor Ahmad, Ex ZH Col M Ikram and Administrator FCCL shools Maj Shahid visiting senior Section of FFS Boys Wah With P & VP"
+            text: "Worthy GM Brig Asim SM, HR Col Noman Bukhari, DM edu Zahoor Ahmad, Ex ZH Col M Ikram and Administrator FCCL shools Maj Shahid visiting senior Section of FFS Boys Wah With P & VP",
+            images: [visit2, visit21, visit22, visit23, visit24]
         },
         {
+            id: 3,
             date: "22 Aug, 2020",
             img: visit3,
             title: "Visit of Honourable Col Noman Bukhari.",
-            text: "Honourable Col Noman Bukhari verifying teacher's data during visit."
+            text: "Honourable Col Noman Bukhari verifying teacher's data during visit.",
+            images: [visit3, visit31]
         }
     ]
     return (
@@ -36,12 +53,13 @@ export const MainVisits = () => {
                     <div className="row bg-white">
                         <div className="col-lg-12 col-md-12 col-12 bg-white">
                             <div className="owl-carousel clients-slider bg-white m-auto">
-                                <VisitComponent data={data} />
+                                <VisitComponent data={data} setSelected={setSelected} setDialog={setDialog} />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <VisitsPicturesDialog dialog={dialog} setDialog={setDialog} selected={selected} setSelected={setSelected} />
         </>
     )
 }
--- src/components/HomeVisits/VisitComponent.jsx
+++ src/components/HomeVisits/VisitComponent.jsx
@@ -1,15 +1,30 @@
 import PropTypes from 'prop-types';
+import './Visits.css'
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
 
-export const VisitComponent = ({ data }) => {
+export const VisitComponent = ({ data, setSelected, setDialog }) => {
     return (
         <>
             {
                 data.map((item) => (
-                    <div key={Math.floor(Math.random()*100000)} className="single-clients" style={{marginBottom: '20px'}}>
+                    <div key={Math.floor(Math.random() * 100000)} className="single-clients" style={{ marginBottom: '20px' }}>
                         <div className="bg-white">
-                            <div style={{ boxShadow: "0px 0px 10px #00000014", transition: "all 0.4s ease", borderRadius: '10px', maxWidth: '350px', marginLeft:'10px', height: '520px' }} className="single-news hover:scale-105 hover:shadow-md">
+                            <div style={{ boxShadow: "0px 0px 10px #00000014", transition: "all 0.4s ease", borderRadius: '10px', maxWidth: '350px', marginLeft: '10px', height: '520px' }} className="single-news hover:scale-105 hover:shadow-md">
                                 <div style={{ width: '350px' }} className="news-head">
-                                    <img className="rounded-md p-0" style={{ transition: "all 0.4s ease", height: "260px", width: "350px" }} src={item.img} alt="#" />
+                                    <div className="image-container cursor-default">
+                                        <img className="rounded-md p-0" style={{ transition: "all 0.4s ease", height: "260px", width: "350px" }} src={item.img} alt="#" />
+                                        <div onClick={()=>{
+                                            setSelected({
+                                                date: item.date,
+                                                title: item.title,
+                                                text: item.text,
+                                                images: item.images
+                                            });
+                                            setDialog(true);
+                                        }} className='detail-button cursor-pointer flex justify-center item-center'>
+                                            <OpenInFullIcon style={{fontSize: "15px", marginRight: "10px"}}/>Show More
+                                        </div>
+                                    </div>
                                 </div>
                                 <div className="news-body" style={{ padding: '0 20px 40px 10px' }}>
                                     <div className="news-content">
@@ -31,10 +46,14 @@ export const VisitComponent = ({ data }) => {
 VisitComponent.propTypes = {
     data: PropTypes.arrayOf(
         PropTypes.shape({
+            id: PropTypes.number,
             date: PropTypes.string.isRequired,
             img: PropTypes.string.isRequired,
             title: PropTypes.string.isRequired,
             text: PropTypes.string.isRequired,
+            images: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired
         })
     ).isRequired,
+    setSelected: PropTypes.func.isRequired,
+    setDialog: PropTypes.func.isRequired,
 };
